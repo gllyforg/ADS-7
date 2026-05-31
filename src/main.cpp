8aa5e21b1160a93df071229828006f3ad19fa764
gllyforg <shilovaksenia2018@mail.ru>
2026-05-31 22:21:08 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,35 +5,34 @@
 
 #include "train.h"
 
-using namespace std;
 int main() {
     const int MAX_N = 200;
     const int STEP = 10;
     // Experiment 1: all lights OFF
-    cout << "ALL OFF:\n";
+    std::cout << "ALL OFF:\n";
     for (int n = 10; n <= MAX_N; n += STEP) {
         Train train;
         for (int i = 0; i < n; i++) train.addCar(false);
         train.getLength();
-        cout << n << "," << train.getOpCount() << endl;
+        std:cout << n << "," << train.getOpCount() << std:endl;
     }
     // Experiment 2: all lights ON
-    cout << "\nALL ON:\n";
+    std:cout << "\nALL ON:\n";
     for (int n = 10; n <= MAX_N; n += STEP) {
         Train train;
         for (int i = 0; i < n; i++) train.addCar(true);
         train.getLength();
-        cout << n << "," << train.getOpCount() << endl;
+        std:cout << n << "," << train.getOpCount() << std:endl;
     }
     // Experiment 3: random lights
-    cout << "\nRANDOM:\n";
+    std:cout << "\nRANDOM:\n";
     mt19937 gen(42);
     uniform_int_distribution<> dis(0, 1);
     for (int n = 10; n <= MAX_N; n += STEP) {
         Train train;
         for (int i = 0; i < n; i++) train.addCar(dis(gen));
         train.getLength();
-        cout << n << "," << train.getOpCount() << endl;
+        std:cout << n << "," << train.getOpCount() << std:endl;
     }
     return 0;
 }
