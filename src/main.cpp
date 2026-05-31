3ad77a5bc293aa6129dd487fbc5202face0dc760
gllyforg <shilovaksenia2018@mail.ru>
2026-05-31 22:07:52 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,45 @@
 // Copyright 2022 NNTU-CS
+#include <vector>
+#include <random>
 #include <iostream>
+
 #include "train.h"
 
+using namespace std;
+
 int main() {
-  Train train;
-  int count = 60; // кол-во вагонов
-
-  while (count--)
-    train.addCar(false);
-
-  std::cout << train.getLength() << std::endl;
-  std::cout << train.getOpCount() << std::endl;
-  return 0;
+    const int MAX_N = 200;
+    const int STEP = 10;
+    
+    // Experiment 1: all lights OFF
+    cout << "ALL OFF:\n";
+    for (int n = 10; n <= MAX_N; n += STEP) {
+        Train train;
+        for (int i = 0; i < n; i++) train.addCar(false);
+        train.getLength();
+        cout << n << "," << train.getOpCount() << endl;
+    }
+    
+    // Experiment 2: all lights ON
+    cout << "\nALL ON:\n";
+    for (int n = 10; n <= MAX_N; n += STEP) {
+        Train train;
+        for (int i = 0; i < n; i++) train.addCar(true);
+        train.getLength();
+        cout << n << "," << train.getOpCount() << endl;
+    }
+    
+    // Experiment 3: random lights
+    cout << "\nRANDOM:\n";
+    mt19937 gen(42);
+    uniform_int_distribution<> dis(0, 1);
+    
+    for (int n = 10; n <= MAX_N; n += STEP) {
+        Train train;
+        for (int i = 0; i < n; i++) train.addCar(dis(gen));
+        train.getLength();
+        cout << n << "," << train.getOpCount() << endl;
+    }
+    
+    return 0;
 }
