3ff0fe66072989b5f8235e3065de98f6cd96ec74
gllyforg <shilovaksenia2018@mail.ru>
2026-05-31 22:13:27 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,9 @@
 #include "train.h"
 
 using namespace std;
-
 int main() {
     const int MAX_N = 200;
     const int STEP = 10;
-    
     // Experiment 1: all lights OFF
     cout << "ALL OFF:\n";
     for (int n = 10; n <= MAX_N; n += STEP) {
@@ -19,7 +17,6 @@ int main() {
         train.getLength();
         cout << n << "," << train.getOpCount() << endl;
     }
-    
     // Experiment 2: all lights ON
     cout << "\nALL ON:\n";
     for (int n = 10; n <= MAX_N; n += STEP) {
@@ -28,18 +25,15 @@ int main() {
         train.getLength();
         cout << n << "," << train.getOpCount() << endl;
     }
-    
     // Experiment 3: random lights
     cout << "\nRANDOM:\n";
     mt19937 gen(42);
     uniform_int_distribution<> dis(0, 1);
-    
     for (int n = 10; n <= MAX_N; n += STEP) {
         Train train;
         for (int i = 0; i < n; i++) train.addCar(dis(gen));
         train.getLength();
         cout << n << "," << train.getOpCount() << endl;
     }
-    
     return 0;
 }
