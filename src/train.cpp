63b6b3ab629de4b83e361834695446ab86f39f4e
gllyforg <shilovaksenia2018@mail.ru>
2026-05-31 22:52:56 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,56 +1,67 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
-
 Train::Train() : countOp(0), first(nullptr) {}
 
 Train::~Train() {
-    if (!first) return;
-    Car* current = first;
-    Car* nextCar = nullptr;
-    do {
-        nextCar = current->next;
-        delete current;
-        current = nextCar;
-    } while (current != first);
+  if (!first) return;
+  Car* current = first->next;
+  while (current != first) {
+    Car* nextCar = current->next;
+    delete current;
+    current = nextCar;
+  }
+  delete first;
 }
 
 void Train::addCar(bool light) {
-    Car* newCar = new Car;
-    newCar->light = light;
-    if (!first) {
-        first = newCar;
-        first->next = first;
-        first->prev = first;
-    } else {
-        Car* last = first->prev;
-        last->next = newCar;
-        newCar->prev = last;
-        newCar->next = first;
-        first->prev = newCar;
-    }
+  Car* newCar = new Car{light, nullptr, nullptr};
+  if (!first) {
+    first = newCar;
+    first->next = first;
+    first->prev = first;
+  } else {
+    Car* last = first->prev;
+    last->next = newCar;
+    newCar->prev = last;
+    newCar->next = first;
+    first->prev = newCar;
+  }
 }
 
 int Train::getLength() {
-    if (!first) return 0;
-    countOp = 0;
-    // Сначала выключаем свет ВСЕГДА в первом вагоне
-    first->light = false;
+  if (!first) return 0;
+  
+  first->light = true;
+  Car* current = first;
+  int steps = 0;
+  
+  while (true) {
+    current = current->next;
+    steps++;
     countOp++;
-    Car* current = first->next;
-    countOp++;
-    int length = 1;
-    while (current != first) {
-        length++;
+    
+    if (current->light) {
+      current->light = false;
+      
+      for (int i = 0; i < steps; ++i) {
+        current = current->prev;
+        countOp++;
+      }
+      
+      if (!first->light) {
+        return steps;
+      }
+      
+      for (int i = 0; i < steps; ++i) {
         current = current->next;
         countOp++;
+      }
+      
+      steps = 0;
     }
-    return length;
+  }
 }
 
-int Train::getOpCount() {
-    return countOp;
-}
-
-void Train::resetOpCount() {
-    countOp = 0;
+int Train::getOpCount() const {
+  return countOp;
 }
