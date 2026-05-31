9581f510312b70db65aebf07ecb85bc108a82dbd
gllyforg <shilovaksenia2018@mail.ru>
2026-05-31 22:53:29 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -8,15 +8,22 @@ class Train {
     bool light; // состояние лампочки
     Car *next;
     Car *prev;
+    bool light;
+    Car* next;
+    Car* prev;
   };
   int countOp; // счетчик шагов (число переходов из вагона в вагон)
   Car *first; // точка входа в поезд (первый вагон)
+  int countOp;
+  Car* first;
  public:
   Train();
-  ~Train();
   void addCar(bool light); // добавить вагон с начальным состоянием лампочки
   int getLength();          // вычислить длину поезда
   int getOpCount();         // вернуть число переходов (из вагона в вагон)
-  void resetOpCount();
+  ~Train();
+  void addCar(bool light);
+  int getLength();
+  int getOpCount() const;
 };
 #endif  // INCLUDE_TRAIN_H_
